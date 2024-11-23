1efd3a6a1e47978c47f49c5268141775399e515e
yelog <yelogeek@gmail.com>
2024-11-23 23:21:53 +0800

    feat: add button style

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use floem::{
     animate::Animation,
     peniko::Color,
     reactive::{create_rw_signal, RwSignal, SignalGet, SignalUpdate},
+    style::{CursorStyle, Style},
     views::*,
     IntoView,
 };
@@ -37,11 +38,28 @@ fn main() {
 fn app_view() -> impl IntoView {
     let view = create_rw_signal(ViewSwitcher::One);
     let start_text = create_rw_signal("".to_string());
+    let primary_color = Color::rgb8(0x16, 0x77, 0xFF);
+
+    let hover_bg_color = Color::rgb8(0x40, 0x96, 0xFF); // 悬停时背景色
 
     h_stack((v_stack((
         button("New Connection")
             .action(move || view.update(|which| which.toggle()))
-            .style(|s| s.width_full()),
+            .style(move |_cx| {
+                Style::default()
+                    .background(primary_color) // 正常背景色
+                    .color(Color::WHITE) // 字体颜色
+                    .height(32.0)
+                    .width_full()
+                    .font_size(14.0)
+                    .padding_horiz(15.0)
+                    .padding_vert(4.0)
+                    .cursor(CursorStyle::Pointer)
+                    .border(0)
+                    .hover(|_cx| {
+                        Style::default().background(hover_bg_color) // 悬停时背景色
+                    })
+            }),
         text_input(start_text)
             .placeholder("Search Connection")
             .style(|s| s.width_full()),
@@ -49,13 +67,13 @@ fn app_view() -> impl IntoView {
             .style(|s| s.border(1.0).border_radius(5.0)),
     ))
     .style(|s| {
-        s.width(200.0) // 设置 v_stack 的宽度为 100
+        s.width(200.0) // 设置 v_stack 的宽度为 200
             .height_full()
             .padding(10.0) // 添加填充以避免被遮挡
             .items_center()
             // .justify_center()
             .gap(20.0)
-            .border_right(1.)
+            .border_right(1.0)
             .border_color(Color::BLACK.multiply_alpha(0.2))
     }),))
     .style(|s| s.width_full().height_full().gap(20.0))
@@ -78,4 +96,3 @@ fn view_two(view: RwSignal<ViewSwitcher>) -> impl IntoView {
             .justify_center()
     })
 }
-
